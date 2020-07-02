631ded2353ea418f1c35a6e5a1eae07907fbfc8c
Anna Shafeeva <anna_shafeeva@epam.com>
2020-07-02 22:23:16 +0400

    extended-repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let add = '';
+  if (options.hasOwnProperty('addition')) {
+    add = (options['addition'] + options['additionSeparator']).repeat(options['additionRepeatTimes'] - 1) + options['addition'];
+  }
+
+  let sep = '+';
+  if (options.hasOwnProperty('separator')) {
+    sep = options['separator'];
+  }
+
+  return (str + add + sep).repeat(options['repeatTimes'] - 1) + str + add;
 };
   
