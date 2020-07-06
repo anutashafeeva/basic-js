8d6f67ee23b95a08cd1d5c2d093ba8d9df728517
Anna Shafeeva <anna_shafeeva@epam.com>
2020-07-06 20:24:25 +0400

    vigenere-cipher done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction) {
+    if (direction === undefined || direction === true) {
+      this.direction = true;
+    } else {
+      this.direction = false;
+    }
+  }
+
+  encrypt(str, key) {
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let res = '';
+    let pos = 0;
+    for(let i = 0; i < str.length; i++) {
+      if (alphabet.indexOf(str[i]) >= 0) {
+      let strChar = str[i];
+      let keyChar = key[pos % key.length];
+      res += alphabet.charAt((alphabet.indexOf(strChar) + alphabet.indexOf(keyChar)) % alphabet.length);
+      pos++;
+      } else {
+        res += str[i];
+      }
+    }
+
+    return this.direction ? res : res.split('').reverse().join('');
+  }
+
+  decrypt(str, key) {
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let res = '';
+    let pos = 0;
+    for(let i = 0; i < str.length; i++) {
+      if (alphabet.indexOf(str[i]) >= 0) {
+      let strChar = str[i];
+      let keyChar = key[pos % key.length];
+      res += alphabet.charAt((alphabet.indexOf(strChar) + alphabet.length - alphabet.indexOf(keyChar)) % alphabet.length);
+      pos++;
+      } else {
+        res += str[i];
+      }
+    }
+
+    return this.direction ? res : res.split('').reverse().join('');
   }
 }
 
